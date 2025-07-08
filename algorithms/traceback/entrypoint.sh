da619c29ca247c3a42f1a392cadac1f3d83c519b
Nugine <nugine@foxmail.com>
2025-07-08 15:10:02 +0800

    use traceback-A8

--- algorithms/traceback/entrypoint.sh
+++ algorithms/traceback/entrypoint.sh
@@ -1,3 +1,3 @@
 #!/bin/bash -ex
-export ALGORITHM=${ALGORITHM:-traceback-A7}
+export ALGORITHM=${ALGORITHM:-traceback-A8}
 LOGURU_COLORIZE=0 .venv/bin/python -m rcabench_platform.v2.online.run_exp_platform run
