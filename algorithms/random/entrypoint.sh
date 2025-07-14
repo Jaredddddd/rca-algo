5a0cda19e3730ecfe6da60a4f54edd45c6a6e0bd
Nugine <nugine@foxmail.com>
2025-07-14 17:11:51 +0800

    rcabench-platform v0.3

--- algorithms/random/entrypoint.sh
+++ algorithms/random/entrypoint.sh
@@ -1,3 +1,3 @@
 #!/bin/bash -ex
 export ALGORITHM=${ALGORITHM:-random}
-LOGURU_COLORIZE=0 .venv/bin/python -m rcabench_platform.v2.online.run_exp_platform run
+LOGURU_COLORIZE=0 .venv/bin/python main.py container run
