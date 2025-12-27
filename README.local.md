ee8a082ff5e30dd774af74498ddb29b77eaa0dca
YifanYang6 <yifanyang6@link.cuhk.edu.cn>
2025-12-27 08:19:13 +0800

    add workspace; update submodule
    Signed-off-by: YifanYang6 <yifanyang6@link.cuhk.edu.cn>

---
+++ README.local.md
@@ -0,0 +1,9 @@
+```sh
+uv run --package baro python algorithms/baro/main.py eval batch -a baro  -d rcabench
+
+uv run --package shapleyiq python algorithms/shapleyiq/main.py eval batch -a shapleyiq  -a microrank -a microhecl -a microrca  -d rcabench
+
+uv run --package  nezha python algorithms/nezha/main.py eval batch -a nezha -d rcabench
+
+uv run main.py eval perf-report rcabench
+```
---
+++ main.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env -S uv run -s
+from rcabench_platform.v2.algorithms.spec import Algorithm, global_algorithm_registry
+from rcabench_platform.v2.cli.main import main
+
+if __name__ == "__main__":
+    registry = global_algorithm_registry()
+    registry["baro"] = Algorithm
+    registry["shapleyiq"] = Algorithm
+    registry["ton"] = Algorithm
+    registry["microrank"] = Algorithm
+    registry["microhecl"] = Algorithm
+    registry["microrca"] = Algorithm
+
+    main(enable_builtin_algorithms=False)
+
